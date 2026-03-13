b694d9db9ff58c2cc4802efcda0cae6e1fb4ef28
kevinsoler313 <kevinsoler313@gmail.com>
2026-03-12 19:18:24 -0500

    Actualizar main

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from routers.partida_router import router as partida_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configuración de CORS para permitir que el frontend se comunique con el backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En desarrollo permitimos todo. En producción, la URL de tu frontend.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(partida_router, prefix="/partida")
